337f4199c4f459842bf2105c00e2cae68a661b3f
Myika Marshall <myika.marshall@gmail.com>
2021-10-20 23:26:57 -0500

    1/3 done

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,16 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+function addItem(item){
+  console.log('in addItem');
+  basket.push(item)
+  if (basket.length >= [0]){
+    return true;}
+}
+addItem('Kleenex');
+addItem('String Cheese');
+addItem('Bentgo Boxes');
+
+console.log('I have these items in my basket:', basket);
+console.log(addItem());
